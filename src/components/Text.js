0191fe3bbc6bdcff6d7c0832b8d1a2c28a94f200
unknown <hh4518@gmail.com>
2021-07-05 19:30:25 +0900

    text style work done

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,31 +2,9 @@
 import React from 'react';
 
 const Text = (props) => {
-  const { strong, fontSize, lineHeight, textAlign, color, children } = props;
-  const styles = {
-    strong,
-    fontSize,
-    lineHeight,
-    textAlign,
-    color,
-  };
+  const { children } = props;
 
-  return <p {...styles}>{children}</p>;
+  return <p className="comments">{children}</p>;
 };
-
-Text.defaultProps = {
-  fontSize: '16px',
-  lineHeight: '1.25',
-  textAlign: 'left',
-  color: '#343a40',
-};
-
-// const TextStyle = styled.p`
-//   font-size: ${(props) => props.fontSize};
-//   line-height: ${(props) => props.lineHeight};
-//   color: ${(props) => props.color};
-//   text-align: ${(props) => props.textAlign};
-//   ${(props) => props.strong && 'font-weight: 700'};
-// `;
 
 export default Text;
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,29 +2,9 @@
 import React from 'react';
 
 const Title = (props) => {
-  const { fontSize, lineHeight, textAlign, color, children } = props;
-  const styles = {
-    fontSize,
-    lineHeight,
-    textAlign,
-    color,
-  };
+  const { children } = props;
 
-  return <h2 {...styles}>{children}</h2>;
+  return <h2 className="title">{children}</h2>;
 };
-
-Title.defaultProps = {
-  fontSize: '16px',
-  lineHeight: '1.25',
-  textAlign: 'left',
-  color: '#343a40',
-};
-
-// const TitleStyle = styled.h2`
-//   font-size: ${(props) => props.fontSize};
-//   line-height: ${(props) => props.lineHeight};
-//   text-align: ${(props) => props.textAlign};
-//   font-weight: 700;
-// `;
 
 export default Title;
