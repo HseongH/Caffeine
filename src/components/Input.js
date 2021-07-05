98f76e04b517c91233f0489234829c2fc7a70e00
unknown <hh4518@gmail.com>
2021-07-05 19:25:29 +0900

    input style work done

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,46 +2,14 @@
 import React from 'react';
 
 const Input = (props) => {
-  const { width, margin, placeholder, type } = props;
-  const styles = {
-    width,
-    margin,
-  };
+  const { placeholder, type } = props;
 
-  return <input className="input--text" {...styles} type={type} placeholder={placeholder} />;
+  return <input className="input--text" type={type} placeholder={placeholder} />;
 };
 
 Input.defaultProps = {
-  width: '100%',
   type: 'text',
   placeholder: '텍스트를 작성해주세요.',
 };
-
-// const InputStyle = styled.input`
-//   --phColor: #6c757d;
-//   width: ${(props) => props.width};
-//   background: #eee;
-//   border: 1px solid #ccc;
-//   border-radius: 10px;
-//   box-sizing: border-box;
-//   padding: 10px 12px;
-//   ${(props) => props.margin && `margin: ${props.margin}`};
-
-//   &::placeholder {
-//     color: --phColor;
-//   }
-
-//   &::-webkit-input-placeholder {
-//     color: --phColor;
-//   }
-
-//   &:-ms-input-placeholder {
-//     color: --phColor;
-//   }
-
-//   &:focus {
-//     outline: none;
-//   }
-// `;
 
 export default Input;
